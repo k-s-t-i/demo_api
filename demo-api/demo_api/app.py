8252d95d4f64c0e2c1e501a5c32ae18b3f42943f
Benjamin Taiwo <taiwo@yedite.ch>
2022-02-19 02:24:01 +0100

    Add: btc_eur calculating service

---
+++ demo-api/demo_api/app.py
@@ -0,0 +1,35 @@
+import logging
+from fastapi import FastAPI
+from pydantic import BaseModel
+from decimal import Decimal
+from demo_api.btc_eur_calculator_service import calculate_btceur_price
+app = FastAPI(title="Bitstack Demo API")
+
+log = logging.getLogger(__name__)
+
+
+class RequestModel(BaseModel):
+    amount: Decimal
+
+
+# class ResponseModel(BaseModel):
+#     timestamp: str = ""
+#     price: Union[float, str] = ""
+#     quantity: str = ""
+
+
+@app.get("/")
+def root_get():
+    return {"description": "Hello coder!"}
+
+@app.post("/btceur")
+async def btc_eur_price(btc_ask: RequestModel):
+    try:
+        if btc_ask.amount <= Decimal(0):
+            return 0
+    except ValueError:
+        log.error()
+        return "Please provide a valid btc ask"
+
+    return await calculate_btceur_price(btc_ask.amount)
+
---
+++ demo-api/demo_api/btc_eur_calculator_service.py
@@ -0,0 +1,38 @@
+import logging
+import websockets
+import json
+from decimal import Decimal
+
+log = logging.getLogger(__name__)
+async def calculate_btceur_price(btc_ask: Decimal) -> dict:
+    if btc_ask <= 0.0:
+        log.error(f"Invalid btc_ask: {btc_ask}")
+        raise ValueError(f"btc_ask should be greater than 0, but received {btc_ask} instead.")
+    remainder_btc = btc_ask
+    result = 0
+    msgs = {"event": "bts:subscribe", "data": {"channel": "order_book_btceur"}}
+    msgs = json.dumps(msgs)
+    uri = "wss://ws.bitstamp.net"
+    log.info(f"Connecting to bitstamp websocket at {uri} ...")
+    async with websockets.connect(uri) as websocket:
+        await websocket.send(msgs)
+
+        async for message in websocket:
+            response_json = json.loads(message)
+            response_data = response_json['data'].get('asks', [])
+            # print(response_data)
+            if len(response_data) > 0:
+                for ask in response_data:
+                    btc = Decimal(ask[1])
+                    if ask == response_data[len(response_data)-1] and btc < remainder_btc:
+                        return {'btc_ask': "btc too large, total_price not available"}
+                    if btc <= remainder_btc:
+                        result += Decimal(ask[0])*btc
+                        remainder_btc -= btc
+                    else:
+                        result += Decimal(ask[0])*btc
+                        break
+                print(result)
+                return {'btc_ask': result}
+
+
